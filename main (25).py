2aa84bc0e8203780778be2fe0c2a9b0d562bd964
Zaharovf <117991204+Zaharovf@users.noreply.github.com>
2023-04-14 15:29:10 +0300

    14/04/23

---
+++ main (25).py
@@ -0,0 +1,18 @@
+a = int(input("Введите число "))
+b = 0
+while a >b:
+    print(b)
+    b +=1
+#№7
+a , b , c = int(input("Введите число ")), 0 ,1 
+while b < a:
+    b += 1/c 
+    c +=1
+    print(b , " + ", end = "")
+print("0 = " , a)
+#№8
+a = int(input("Введите число "))
+b = 0
+while b**2 <a:
+    b +=1
+print(b)
